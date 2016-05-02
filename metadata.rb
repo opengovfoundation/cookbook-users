9340e8f2f35f5ac029ee349337c5ebd4a2b7fd06
Bill Hunt <bill@krues8dr.com>
2016-05-02 12:38:20 -0400

    Version bump.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'developers@opengovfoundation.org'
 license          'CC0'
 description      'Installs/Configures user accounts'
 long_description 'Installs/Configures user accounts'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'chef-vault'
 depends 'sudo'
