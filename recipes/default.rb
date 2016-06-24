ae3a584aac3380d7d562fd1f39c0dbff93d0e73c
Bill Hunt <bill@krues8dr.com>
2016-06-24 13:48:53 -0400

    Using snake case instead of camel case.

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,7 +25,7 @@ include_recipe 'chef-vault::default'
 
 # Create a list of the users. Apparently you can't just iterate over the data
 # bag's contents directly.
-serverUsers = data_bag('users').select do |key|
+server_users = data_bag('users').select do |key|
   !key.end_with?('_keys')
 end
 
@@ -36,71 +36,71 @@ group 'staff' do
 end
 
 # Keep a list of all ssh keys for our deploy user later.
-deploySshKeys = []
+deploy_ssh_keys = []
 
 # Function for creating users and their SSH keys.
-def createUser(userName, fullUser)
+def create_user(user_name, full_user)
 
   # Create the user's account.
-  user userName do
-    comment fullUser['comment']
-    password fullUser['password']
-    shell fullUser['shell']
+  user user_name do
+    comment full_user['comment']
+    password full_user['password']
+    shell full_user['shell']
     group 'staff'
-    home "/home/#{userName}"
+    home "/home/#{user_name}"
   end
 
   # Create the user's home directory if it doesn't exist.
   # This is necessary on Ubuntu and some other distros.
-  directory "/home/#{userName}" do
-    owner userName
+  directory "/home/#{user_name}" do
+    owner user_name
     group 'staff'
     mode '0755'
     action :create
   end
 
   # Create the ssh directory.
-  directory "/home/#{userName}/.ssh" do
-    owner userName
+  directory "/home/#{user_name}/.ssh" do
+    owner user_name
     group 'staff'
     mode '0755'
     action :create
   end
 
   # Add our public key to the user's authorized keys.
-  file "/home/#{userName}/.ssh/authorized_keys" do
-    content fullUser['ssh_keys'].join("\n")
+  file "/home/#{user_name}/.ssh/authorized_keys" do
+    content full_user['ssh_keys'].join("\n")
     mode '0644'
-    owner userName
+    owner user_name
     group 'staff'
   end
 end
 
 # Loop over our users.
-serverUsers.each do |userName|
+server_users.each do |user_name|
   # Get the users' real data from the vault.
-  fullUser = chef_vault_item('users', userName)
+  full_user = chef_vault_item('users', user_name)
 
-  createUser(userName, fullUser)
+  create_user(user_name, full_user)
 
   # Add our ssh keys to the list for our deploy user.
-  deploySshKeys.concat(fullUser['ssh_keys']);
+  deploy_ssh_keys.concat(full_user['ssh_keys']);
 end
 
 # Create our deploy user.
-deployUser = {
+deploy_user = {
   'comment' => 'Deploy User',
   'password' => nil,
   'shell' => '/bin/bash',
-  'ssh_keys' => deploySshKeys
+  'ssh_keys' => deploy_ssh_keys
 }
 
-createUser('deploy', deployUser)
+create_user('deploy', deploy_user)
 
 # Create sysadmin group. Add our users to it.
 group 'sysadmin' do
   action :create
-  members serverUsers
+  members server_users
 end
 
 # The sudo recipe gives privileges to sysadmin group by default.
